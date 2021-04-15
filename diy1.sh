211a95edeac1276451f8d2ef6b4c4432cbeb1878
xiangfeidexiaohuo <jsjson@163.com>
2021-04-16 03:03:28 +0800

    delete ./package/lean/v2ray-plugin
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy1.sh
+++ diy1.sh
@@ -12,6 +12,7 @@ rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/luci-app-jd-dailybonus
 rm -rf ./package/lean/luci-app-diskman
 rm -rf ./package/lean/qBittorrent
+rm -rf ./package/lean/v2ray-plugin
                
 echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
 echo -e "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
