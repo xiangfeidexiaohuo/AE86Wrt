15a22d46d61d026e3a983e94ce2d75cca41ff27a
xiangfeidexiaohuo <jsjson@163.com>
2021-06-23 00:53:32 +0800

    Update clang build
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy_clang.sh
+++ diy_clang.sh
@@ -6,10 +6,12 @@ rm -rf clang-tmp
 ./build-llvm.py \
 	--build-stage1-only \
 	--install-stage1-only \
+	--lto "thin" \
 	--projects "clang;lld;polly" \
 	--targets "ARM;AArch64;X86" \
 	--install-folder "clang-tmp" \
-	--clang-vendor "AE86-$(date +%Y%m%d)" 
+	--clang-vendor "AE86-$(date +%Y%m%d)" \
+	--additional-build-arguments "CLANG_REPOSITORY_STRING=GitHub.com/xiangfeidexiaohuo" 
 
 
 #rm -rf binutils-tmp
