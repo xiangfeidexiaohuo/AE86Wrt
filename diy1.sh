f6121ca5cda649eda87cb7d505e633b6e18669a0
xiangfeidexiaohuo <jsjson@163.com>
2021-07-01 22:26:42 +0800

    Script upgrade
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy1.sh
+++ diy1.sh
@@ -5,13 +5,11 @@
 #=================================================
 ##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
-#rm -rf ./package/lean/luci-app-sfe
 rm -rf ./package/lean/luci-app-syncdial
 rm -rf ./package/lean/luci-lib-docker
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/luci-app-jd-dailybonus
 rm -rf ./package/lean/luci-app-diskman
-#rm -rf ./package/lean/qBittorrent
                
 echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
 echo -e "src-git nas https://github.com/linkease/nas-packages.git;master" >> feeds.conf.default
@@ -19,14 +17,5 @@ echo -e "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.d
 echo -e "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 echo -e "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter" >> feeds.conf.default
 echo -e "src-git OpenClash https://github.com/vernesong/OpenClash" >> feeds.conf.default
+echo -e "src-git jerryk https://github.com/jerrykuku/openwrt-package" >> feeds.conf.default
 
-rm -rf ./package/yaodao
-##dnsfilter
-git clone -b main --depth 1 https://github.com/garypang13/luci-app-dnsfilter.git ./package/yaodao/luci-app-dnsfilter
-
-##jerrykuku
-git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git ./package/yaodao/luci-theme-argon
-git clone -b master --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git ./package/yaodao/luci-app-argon-config
-git clone -b master --depth 1 https://github.com/jerrykuku/luci-app-vssr.git ./package/yaodao/luci-app-vssr
-git clone -b master --depth 1 https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/yaodao/luci-app-jd-dailybonus
-#git clone -b master --depth 1 https://github.com/jerrykuku/lua-maxminddb.git ./package/yaodao/lua-maxminddb
--- diy2.sh
+++ diy2.sh
@@ -19,8 +19,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci-nginx/Makefile
 
 
 ##加入作者信息
