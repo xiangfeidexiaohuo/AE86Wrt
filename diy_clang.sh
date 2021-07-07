9bd5be95f9e8ee5fb7b117396ce82968dea61412
xiangfeidexiaohuo <jsjson@163.com>
2021-07-07 16:54:45 +0800

    update clang
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy_clang.sh
+++ diy_clang.sh
@@ -6,7 +6,6 @@ rm -rf clang-tmp
 ./build-llvm.py \
 	--build-stage1-only \
 	--install-stage1-only \
-	--lto "thin" \
 	--projects "clang;lld;polly" \
 	--targets "ARM;AArch64;X86" \
 	--install-folder "clang-tmp" \
