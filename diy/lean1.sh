bbe9ada2ea607481d0635f69d07fe74720524a9b
xiangfeidexiaohuo <jsjson@163.com>
2021-09-26 22:03:25 +0800

    up
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean1.sh
+++ diy/lean1.sh
@@ -11,6 +11,7 @@ rm -rf ./package/lean/luci-app-jd-dailybonus
 #rm -rf ./package/lean/luci-app-diskman
 rm -rf ./package/lean/luci-lib-docker
 rm -rf ./package/lean/luci-app-pushbot
+rm -rf ./package/lean/luci-app-netdata
                
 echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
 echo -e "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
