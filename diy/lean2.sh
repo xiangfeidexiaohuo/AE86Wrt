2f4d0a2357b71a3b222a9bd3d70f10afa55f71bf
xiangfeidexiaohuo <jsjson@163.com>
2021-10-27 17:39:36 +0800

    update
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean2.sh
+++ diy/lean2.sh
@@ -11,12 +11,21 @@ rm -rf feeds/packages/net/ariang/*
 rm -rf feeds/luci/applications/luci-app-aria2/*
 rm -rf feeds/packages/utils/docker/*
 rm -rf feeds/packages/utils/dockerd/*
+rm -rf feeds/packages/utils/containerd/*
+rm -rf feeds/packages/utils/runc/*
+rm -rf feeds/luci/applications/luci-app-dawn/*
+rm -rf feeds/packages/net/dawn/*
 
 cp -af feeds/xiangfeidexiaohuo/smartdns/* feeds/packages/net/smartdns/
 cp -af feeds/xiangfeidexiaohuo/aria2-op/ariang/* feeds/packages/net/ariang/
 cp -af feeds/xiangfeidexiaohuo/aria2-op/luci-app-aria2/* feeds/luci/applications/luci-app-aria2/
 cp -af feeds/xiangfeidexiaohuo/docker-op/docker/* feeds/packages/utils/docker/
 cp -af feeds/xiangfeidexiaohuo/docker-op/dockerd/* feeds/packages/utils/dockerd/
+cp -af feeds/xiangfeidexiaohuo/docker-op/containerd/* feeds/packages/utils/containerd/
+cp -af feeds/xiangfeidexiaohuo/docker-op/runc/* feeds/packages/utils/runc/
+cp -af feeds/xiangfeidexiaohuo/luci-app-dawn/* feeds/luci/applications/luci-app-dawn/
+cp -af feeds/xiangfeidexiaohuo/dawn/*  feeds/packages/net/dawn/
+
 
 ###
 rm -rf feeds/packages/libs/glib2/*
