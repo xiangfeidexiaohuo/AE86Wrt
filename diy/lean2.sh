57a3b35c2681e791dbc7aa93e2433169df896f7b
xiangfeidexiaohuo <jsjson@163.com>
2021-11-15 14:18:02 +0800

    update
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean2.sh
+++ diy/lean2.sh
@@ -14,7 +14,6 @@ rm -rf feeds/packages/utils/dockerd/*
 rm -rf feeds/packages/utils/containerd/*
 rm -rf feeds/packages/utils/runc/*
 rm -rf feeds/packages/utils/docker-compose/*
-rm -rf feeds/luci/applications/luci-app-dawn/*
 rm -rf feeds/packages/utils/ntfs-3g/*
 rm -rf feeds/packages/net/zerotier/*
 ##
@@ -26,7 +25,6 @@ cp -af feeds/xiangfeidexiaohuo/docker-op/dockerd/* feeds/packages/utils/dockerd/
 cp -af feeds/xiangfeidexiaohuo/docker-op/containerd/* feeds/packages/utils/containerd/
 cp -af feeds/xiangfeidexiaohuo/docker-op/runc/* feeds/packages/utils/runc/
 cp -af feeds/xiangfeidexiaohuo/docker-op/docker-compose/* feeds/packages/utils/docker-compose/
-cp -af feeds/xiangfeidexiaohuo/luci-app-dawn/* feeds/luci/applications/luci-app-dawn/
 cp -af feeds/xiangfeidexiaohuo/ntfs-3g/*  feeds/packages/utils/ntfs-3g/
 cp -af feeds/xiangfeidexiaohuo/zerotier/*  feeds/packages/net/zerotier/
 
@@ -43,8 +41,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 rm -rf ./package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 svn export https://github.com/xiangfeidexiaohuo/Phicomm-K3_Wireless-Firmware/trunk/brcmfmac4366c-pcie.bin_69027 ./package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
 
-##更改主机名
-#sed -i "s/hostname='OpenWrt'/hostname='AE86'/g" package/base-files/files/bin/config_generate
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
