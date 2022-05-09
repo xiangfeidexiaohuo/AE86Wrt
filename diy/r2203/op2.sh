d6a02443c09922c9733db5566565939585b3e127
xiangfeidexiaohuo <jsjson@163.com>
2022-05-09 13:04:50 +0800

    diy: docker 20.10.15
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/r2203/op2.sh
+++ diy/r2203/op2.sh
@@ -4,15 +4,17 @@
 # jsjson@163.com 
 #=================================================             
 
-# Docker v20.10.14
+# Docker v20.10.15
 rm -rf feeds/packages/utils/docker
 rm -rf feeds/packages/utils/dockerd
 rm -rf feeds/packages/utils/containerd
 rm -rf feeds/packages/utils/runc
+rm -rf feeds/packages/utils/libnetwork
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/docker feeds/packages/utils/docker
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/dockerd feeds/packages/utils/dockerd
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/containerd feeds/packages/utils/containerd
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/runc feeds/packages/utils/runc
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds/packages/utils/libnetwork
 
 ##配置IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
