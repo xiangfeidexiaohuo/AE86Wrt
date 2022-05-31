6b66668d9cba0121d120789f2fcd46a402cd5b3b
xiangfeidexiaohuo <jsjson@163.com>
2022-05-31 13:45:45 +0800

    r2203
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/r2203/op1.sh
+++ diy/r2203/op1.sh
@@ -4,7 +4,7 @@
 # jsjson@163.com 
 #=================================================
 ##添加自己的插件库
-echo "src-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >>  feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+sed -i "1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" feeds.conf.default
+sed -i "2isrc-git passwall https://github.com/xiaorouji/openwrt-passwall" feeds.conf.default
+sed -i "3isrc-git helloworld https://github.com/fw876/helloworld\n" feeds.conf.default
 
