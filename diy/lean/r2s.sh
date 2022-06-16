1092f426f963b81dbac478e5fe8f9f6818e92031
xiangfeidexiaohuo <jsjson@163.com>
2022-06-16 20:33:47 +0800

    r2s: update
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

---
+++ diy/lean/r2s.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#=================================================
+# DIY script
+# jsjson@163.com 
+#=================================================
+
+## R2S专属优化
+# 关闭 eth0 offloading rx/rx
+sed -i '/set_interface_core 4 "eth1"/a\\tset_interface_core 1 "ff150000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
+sed -i '/ff150000.i2c/a\\tset_interface_core 8 "ff160000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
+
+# 交换 LAN/WAN 口
+sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
+#增加风扇调速
+wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/etc/init.d/fa-rk3328-pwmfan
+wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/usr/bin/start-rk3328-pwm-fan.sh
+
+
