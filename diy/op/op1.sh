c75312d92fc480e72a2764ae178a9fd19a71cf46
xiangfeidexiaohuo <jsjson@163.com>
2022-07-05 15:58:18 +0800

    update

--- diy/op/op1.sh
+++ diy/op/op1.sh
@@ -5,7 +5,4 @@
 #=================================================
 ##添加自己的插件库
 sed -i "1isrc-git add https://github.com/xiangfeidexiaohuo/op-add" feeds.conf.default
-sed -i "2isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" feeds.conf.default
-sed -i "3isrc-git passwall https://github.com/xiaorouji/openwrt-passwall" feeds.conf.default
-sed -i "4isrc-git helloworld https://github.com/fw876/helloworld\n" feeds.conf.default
-
+sed -i "2isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages\n" feeds.conf.default
--- diy/op/op2.sh
+++ diy/op/op2.sh
@@ -10,11 +10,13 @@ rm -rf feeds/packages/utils/dockerd
 rm -rf feeds/packages/utils/containerd
 rm -rf feeds/packages/utils/runc
 rm -rf feeds/packages/utils/libnetwork
+rm -rf feeds/packages/utils/tini
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/docker feeds/packages/utils/docker
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/dockerd feeds/packages/utils/dockerd
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/containerd feeds/packages/utils/containerd
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/runc feeds/packages/utils/runc
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds/packages/utils/libnetwork
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/tini feeds/packages/utils/tini
 
 ##配置IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
