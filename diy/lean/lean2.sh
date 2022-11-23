f9f4d0b8f2c05c2a12163934d650958fc75783b3
xiangfeidexiaohuo <jsjson@163.com>
2022-11-23 09:47:17 +0800

    update
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/lean2.sh
+++ diy/lean/lean2.sh
@@ -98,3 +98,6 @@ svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/pac
 ##
 echo -e "CONFIG_BLK_DEV_NVME=y" >> target/linux/x86/config-6.0
 
+sed -i '/High Performing Mode/d' package/feeds/xiangfeidexiaohuo/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
+sed -i '/High Performing Mode/d' package/feeds/luci/luci-app-firewall/luasrc/model/cbi/firewall/zones.lua
+
