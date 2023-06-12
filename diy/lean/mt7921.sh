1955af17c6877f98f7624aa5577623f1632b5045
xiangfeidexiaohuo <jsjson@163.com>
2023-06-12 17:55:01 +0800

    fix mt7921-6.1
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/mt7921.sh
+++ diy/lean/mt7921.sh
@@ -12,3 +12,6 @@ cp -af feeds/xiangfeidexiaohuo/patch/mt76 package/kernel/
 ##快速出来WiFi
 sed -i "s/exit\ 0/wifi\ config\ \&\&\ wifi\nexit\ 0/g"  target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
+##net:mt76:fix mt76 build failure
+cp -af feeds/xiangfeidexiaohuo/patch/mt7921/* target/linux/generic/pending-6.1/
+
