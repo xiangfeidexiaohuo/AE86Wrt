053ef828f47966ecea351c3d9e3963463ce837b7
xiangfeidexiaohuo <jsjson@163.com>
2023-06-12 17:25:46 +0800

    mt7921 快速出来WiFi
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/mt7921.sh
+++ diy/lean/mt7921.sh
@@ -9,3 +9,6 @@
 rm -rf package/kernel/mt76
 cp -af feeds/xiangfeidexiaohuo/patch/mt76 package/kernel/
 
+##快速出来WiFi
+sed -i "s/exit\ 0/wifi\ config\ \&\&\ wifi\nexit\ 0/g"  target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
