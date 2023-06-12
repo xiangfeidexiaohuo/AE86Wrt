ddd335f3fb07e72047aab1fda1224a52bd705b0f
xiangfeidexiaohuo <jsjson@163.com>
2023-06-12 17:10:02 +0800

    fan
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/r5s.sh
+++ diy/lean/r5s.sh
@@ -4,12 +4,6 @@
 # jsjson@163.com 
 #=================================================
 
-rm -rf target/linux/rockchip/armv8/base-files/etc/init.d/fa-fancontrol
-rm -rf target/linux/rockchip/armv8/base-files/etc/rc.d/S21fa-fancontrol
-rm -rf target/linux/rockchip/armv8/base-files/usr/bin/cputemp.sh
-rm -rf target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
-rm -rf target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh
-
 
 ##MT7921
 rm -rf package/kernel/mt76
