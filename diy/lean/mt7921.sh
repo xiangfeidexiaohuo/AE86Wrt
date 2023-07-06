e6b761c89a111e516c2e501b88c1730ca625ae60
xiangfeidexiaohuo <jsjson@163.com>
2023-07-07 00:32:33 +0800

    7921 update
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/mt7921.sh
+++ diy/lean/mt7921.sh
@@ -4,17 +4,13 @@
 # jsjson@163.com 
 #=================================================
 
-
-##MT7921
-rm -rf package/kernel/mt76
-cp -af feeds/xiangfeidexiaohuo/patch/mt76 package/kernel/
-
 ##快速出来WiFi
 sed -i "s/exit\ 0/wifi\ config\ \&\&\ wifi\nexit\ 0/g"  target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 sed -i "s/exit\ 0/wifi\ config\ \&\&\ wifi\nexit\ 0/g"  target/linux/x86/base-files/etc/board.d/02_network
 
+##MT7921
+rm -rf package/kernel/mt76
+svn export https://github.com/immortalwrt/immortalwrt/trunk/package/kernel/mt76 ./package/kernel/mt76
 
-##net:mt76:fix mt76 build failure
-# cp -af feeds/xiangfeidexiaohuo/patch/mt7921/* target/linux/generic/pending-6.1/
-
+# cp -af feeds/xiangfeidexiaohuo/patch/mt76 package/kernel/
