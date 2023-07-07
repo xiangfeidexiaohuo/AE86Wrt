bdfb81584c8103d89aa9feb86d97e541df4a636d
xiangfeidexiaohuo <jsjson@163.com>
2023-07-07 19:52:52 +0800

    little
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/mt7921.sh
+++ diy/lean/mt7921.sh
@@ -12,5 +12,8 @@ sed -i "s/exit\ 0/wifi\ config\ \&\&\ wifi\nexit\ 0/g"  target/linux/x86/base-fi
 ##MT7921
 rm -rf package/kernel/mt76
 svn export https://github.com/immortalwrt/immortalwrt/trunk/package/kernel/mt76 ./package/kernel/mt76
-
 # cp -af feeds/xiangfeidexiaohuo/patch/mt76 package/kernel/
+
+##升级默认mt76版本
+# sed -i "s/969b7b5ebd129068ca56e4b0d831593a2f92382f/f704e4f83c6fd21fb39046fa328efb51bee6383b/g" package/kernel/mt76/Makefile
+# sed -i "s/2023-05-13/2023-07-04/g" package/kernel/mt76/Makefile
