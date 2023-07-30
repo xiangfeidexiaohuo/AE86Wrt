b336fc17da3044408167a20a5e4231a1d9cdc3cf
xiangfeidexiaohuo <jsjson@163.com>
2023-07-30 22:21:43 +0800

    ++++++++++++++++++++++++
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/op/op2.sh
+++ diy/op/op2.sh
@@ -4,6 +4,20 @@
 # jsjson@163.com 
 #=================================================             
 
+# Docker v20.10.17
+rm -rf feeds/packages/utils/docker
+rm -rf feeds/packages/utils/dockerd
+rm -rf feeds/packages/utils/containerd
+rm -rf feeds/packages/utils/runc
+rm -rf feeds/packages/utils/libnetwork
+rm -rf feeds/packages/utils/tini
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/docker feeds/packages/utils/docker
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/dockerd feeds/packages/utils/dockerd
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/containerd feeds/packages/utils/containerd
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/runc feeds/packages/utils/runc
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds/packages/utils/libnetwork
+svn export https://github.com/coolsnowwolf/packages/trunk/utils/tini feeds/packages/utils/tini
+
 
 ##
 svn export https://github.com/coolsnowwolf/luci/trunk/libs/luci-lib-fs ./package/add/luci-lib-fs
