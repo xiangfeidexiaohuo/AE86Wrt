a3dd2b1c091eed25008552589d95064b8a00342a
xiangfeidexiaohuo <jsjson@163.com>
2023-08-01 01:45:48 +0800

    798x
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

---
+++ diy/mt798x/op1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#=================================================
+# DIY script
+# jsjson@163.com 
+#=================================================
+##添加自己的插件库
+echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
