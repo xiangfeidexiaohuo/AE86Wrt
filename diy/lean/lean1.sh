44acac4cc84a4dca2c2bd677a538ff196eb13c1d
xiangfeidexiaohuo <jsjson@163.com>
2023-11-04 21:20:45 +0800

    update
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -8,5 +8,4 @@ rm -rf ./package/lean/k3screenctrl
 rm -rf ./package/lean/autocore
                
 sed -i "1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" feeds.conf.default
-sed -i "2isrc-git ipkg https://github.com/xiangfeidexiaohuo/op-ipkg\n" feeds.conf.default
 
