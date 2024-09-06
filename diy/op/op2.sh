59d5bbb49dfa0edb1ded680333b1865a2e6a93d7
xiangfeidexiaohuo <jsjson@163.com>
2024-09-06 17:42:29 +0800

    ss-libv
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/op/op2.sh
+++ diy/op/op2.sh
@@ -113,10 +113,10 @@ sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-bypass/luasrc/view/byp
 
 
 ## fix_ss_libv
-rm -rf package/libs/mbedtls
-rm -rf package/libs/uclient
-rm -rf package/libs/ustream-ssl
-cp -af feeds/extraipk/patch/fix_ss_libv/*  package/libs/
+# rm -rf package/libs/mbedtls
+# rm -rf package/libs/uclient
+# rm -rf package/libs/ustream-ssl
+# cp -af feeds/extraipk/patch/fix_ss_libv/*  package/libs/
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
