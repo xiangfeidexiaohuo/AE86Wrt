fa797b9f148f344be217b785bd18f41d97310079
xiangfeidexiaohuo <jsjson@163.com>
2024-10-19 00:24:25 +0800

    lean2305
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/lean/lean0.sh
+++ diy/lean/lean0.sh
@@ -4,6 +4,7 @@
 #=================================================
 ##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
+rm -rf ./package/lean/autocore
                
-sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
+sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
 
