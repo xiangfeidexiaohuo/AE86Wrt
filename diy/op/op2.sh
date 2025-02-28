93260f9e93941d7fafdb9a83a7c122f45cc1004c
xiangfeidexiaohuo <jsjson@163.com>
2025-03-01 03:27:36 +0800

    fixfixfix
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/op/op2.sh
+++ diy/op/op2.sh
@@ -17,23 +17,8 @@ echo -e "msgstr \"魔法网络\"" >> feeds/luci/modules/luci-base/po/zh_Hans/bas
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 ##
-rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
-rm -rf ./feeds/extraipk/theme/luci-app-argon-config-18.06
-rm -rf ./feeds/extraipk/theme/luci-theme-design
-rm -rf ./feeds/extraipk/theme/luci-theme-edge
-rm -rf ./feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./feeds/extraipk/theme/luci-theme-neobird
-rm -rf ./feeds/extraipk/patch/luci-app-turboacc
-
-rm -rf ./package/feeds/extraipk/luci-theme-argon-18.06
-rm -rf ./package/feeds/extraipk/luci-app-argon-config-18.06
-rm -rf ./package/feeds/extraipk/theme/luci-theme-design
-rm -rf ./package/feeds/extraipk/theme/luci-theme-edge
-rm -rf ./package/feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./package/feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./package/feeds/extraipk/theme/luci-theme-neobird
-rm -rf ./package/feeds/extraipk/luci-app-turboacc
+rm -rf ./feeds/2305ipk/patch/luci-app-turboacc
+rm -rf ./package/feeds/2305ipk/luci-app-turboacc
 
 
 ##取消bootstrap为默认主题
@@ -47,7 +32,7 @@ sed -i "s/hostname='.*'/hostname='AE86Wrt'/g" package/base-files/files/bin/confi
 ##加入作者信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='AE86Wrt-$(date +%Y%m%d)'/g"  package/base-files/files/etc/openwrt_release
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By DaoDao'/g" package/base-files/files/etc/openwrt_release
-cp -af feeds/extraipk/patch/diy/banner  package/base-files/files/etc/banner
+cp -af feeds/2305ipk/patch/diy/banner  package/base-files/files/etc/banner
 
 sed -i "2iuci set istore.istore.channel='ae86_daodao'" package/base-files/files/bin/config_generate
 sed -i "3iuci commit istore" package/base-files/files/bin/config_generate
@@ -59,64 +44,16 @@ sed -i "54iLUCI_LANG.zh-tw=\$(LUCI_LANG.zh_Hant)" feeds/luci/luci.mk
 
 ##MosDNS
 # rm -rf feeds/packages/net/mosdns/*
-# cp -af feeds/extraipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
+# cp -af feeds/2305ipk/op-mosdns/mosdns/* feeds/packages/net/mosdns/
 rm -rf feeds/packages/net/v2ray-geodata/*
-cp -af feeds/extraipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
-
-##Adblock
-# rm -rf feeds/luci/applications/luci-app-adblock/*
-# cp -af feeds/extraipk/luci-app-adblock/*  feeds/luci/applications/luci-app-adblock/
-
-##FQ全部调到VPN菜单
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-ssr-plus/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-ssr-plus/luasrc/view/shadowsocksr/*.htm
-
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/passwall/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/model/cbi/passwall/client/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/model/cbi/passwall/server/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/app_update/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/socks_auto_switch/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/global/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/haproxy/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/log/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/node_list/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/rule/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall/luasrc/view/passwall/server/*.htm
-
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/passwall2/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/model/cbi/passwall2/client/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/model/cbi/passwall2/server/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/app_update/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/socks_auto_switch/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/global/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/haproxy/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/log/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/node_list/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/rule/*.htm
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-passwall2/luasrc/view/passwall2/server/*.htm
-
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-vssr/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-vssr/luasrc/model/cbi/vssr/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-vssr/luasrc/view/vssr/*.htm
-
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-openclash/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-openclash/luasrc/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-openclash/luasrc/model/cbi/openclash/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-openclash/luasrc/view/openclash/*.htm
-
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-bypass/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-bypass/luasrc/model/cbi/bypass/*.lua
-sed -i 's/services/vpn/g' package/feeds/extraipk/luci-app-bypass/luasrc/view/bypass/*.htm
+cp -af feeds/2305ipk/op-mosdns/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
 
 
 ## fix_ss_libv
 rm -rf package/libs/mbedtls
 rm -rf package/libs/uclient
 rm -rf package/libs/ustream-ssl
-cp -af feeds/extraipk/patch/fix_ss_libv/*  package/libs/
+cp -af feeds/2305ipk/patch/fix_ss_libv/*  package/libs/
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
