d57a28a7d328d6413d1758fb225debd8d878b4c7
xiangfeidexiaohuo <jsjson@163.com>
2025-03-01 03:02:28 +0800

    fix fix fix
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/op/op1.sh
+++ diy/op/op1.sh
@@ -3,4 +3,4 @@
 # DaoDao's script
 #=================================================
 ##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
+echo -e "\nsrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk" >> feeds.conf.default
