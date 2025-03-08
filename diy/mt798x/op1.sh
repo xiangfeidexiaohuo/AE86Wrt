b45ae6554c27ad1e4b4c077ca918666d685e8a0a
xiangfeidexiaohuo <jsjson@163.com>
2025-03-09 00:11:03 +0800

    fix 798x
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- diy/mt798x/op1.sh
+++ diy/mt798x/op1.sh
@@ -3,4 +3,4 @@
 # DaoDao's script
 #=================================================
 ##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
+sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
