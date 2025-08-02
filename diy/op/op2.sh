53e5d20d99158a5e8e8353d1d8c96841e36683fc
刀刀 <jsjson@163.com>
2025-08-02 12:02:48 +0800

    rust: ci false & config: sync
    Signed-off-by: 刀刀 <jsjson@163.com>

--- diy/op/op2.sh
+++ diy/op/op2.sh
@@ -63,5 +63,9 @@ rm -rf package/libs/uclient
 rm -rf package/libs/ustream-ssl
 cp -af feeds/2305ipk/patch/fix_ss_libv/*  package/libs/
 
+## golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+## rust(ci false)
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
