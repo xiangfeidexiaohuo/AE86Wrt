3a0d153e0d568837bf532261ffd8a71c565394a6
刀刀 <jsjson@163.com>
2025-09-29 02:01:27 +0800

    config lite & sync
    Signed-off-by: 刀刀 <jsjson@163.com>

--- diy/lean/lean01.sh
+++ diy/lean/lean01.sh
@@ -8,5 +8,5 @@ rm -rf ./package/lean/autocore
                
 sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
 
-sed -i 's/luci.git;openwrt-23.05/luci/g' feeds.conf.default
-sed -i 's/luci.git;openwrt-24.10/luci/g' feeds.conf.default
+sed -i 's/luci.git;openwrt-23.05/luci.git/g' feeds.conf.default
+sed -i 's/luci.git;openwrt-24.10/luci.git/g' feeds.conf.default
