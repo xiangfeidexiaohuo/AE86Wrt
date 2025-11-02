1abd22786fa77b64f730934ca9b39c3cce06a0d2
刀刀 <jsjson@163.com>
2025-11-02 14:22:12 +0800

    2025-11: test mtk-6.6
    Signed-off-by: 刀刀 <jsjson@163.com>

---
+++ diy/mt798x/imm1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#=================================================
+# DaoDao's script
+#=================================================
+##添加自己的插件库
+sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
